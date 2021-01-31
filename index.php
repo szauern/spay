205e288b994692de3584aa16643e68fd3107ccb7
szauern <77895752+szauern@users.noreply.github.com>
2021-01-31 15:37:48 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,5 +4,5 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-include 'vendor/autoload.php';
-require 'vendor/szauern/spay/src/SPay.php';
+include '/home/onlytes1/vendor/autoload.php';
+require '/home/onlytes1/vendor/szauern/spay/src/SPay.php';
